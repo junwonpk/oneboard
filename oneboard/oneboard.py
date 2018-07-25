8d71a1360c942885e42f8e57518d3fce26337919
Junwon Park <junwon@orbis.ai>
2018-07-24 22:31:16 -0700

    Demo Scenario

--- oneboard/oneboard.py
+++ oneboard/oneboard.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 from user import User
 import re
 
 user = User.loadUserData()
-lastChapter = 2
+lastChapter = 7
 
 def loadChapters():
     chapters = {}
@@ -22,6 +25,8 @@ def processMessage(message):
     chapters = loadChapters()
     chapter = chapters[user.chapter]
     chapter(message)
+    # body text is message.body["text"]
+    user.chapter += 1
     user.saveUserData
 
 # @respond_to('My name is (.*)')
@@ -35,36 +40,25 @@ def chapter0(message):
     message.reply("Nice to meet you, {}! Welcome to Microsoft!".format(user.name))
     message.reply("I'm Oneboard, and I'll be guiding you through Microsoft resources until you feel fully onboarded!")
     message.reply("It seems like you are {} in {}. Does this sound right?".format(user.role,user.team))
-    user.chapter += 1
 
 def chapter1(message):
-    message.reply("Great! Tell me your name.")
-    user.chapter += 1
+    message.reply("Let’s get you ramped up on Microsoft One Engineering System (1ES).")
+    message.reply("Go read this document: [link], and let me know once you have done this.")
 
 def chapter2(message):
-    print(message)
-    print(message.body["text"])
-    user.name = extract_name(message.body["text"])
-    message.reply("Your name is {}".format(user.name))
+    message.reply("What do you not understand?")
 
-def extract_name(message):
-    name = []
-    name = re.findall('.*is\s(\w+).*', message.lower())
-    if not name:
-        name = re.findall('.*im\s(\w+).*', message.lower())
-    if not name:
-        name = re.findall('.*\'m\s(\w+).*', message.lower())
-    if not name:
-        name = re.findall('.*am\s(\w+).*', message.lower())
-    if not name:
-        name = re.findall('.*call\sme\s(\w+).*', message.lower())
-    if not name:
-        name = re.findall('.*known\sas\s(\w+).*', message.lower())
-    if not name:
-        name = re.findall('.*\'s\s(\w+).*', message.lower())
-    if not name and len(message.split()) == 1:
-        return message.title()
-    if name:
-        return name[0].title()
-    else:
-        return []
+def chapter3(message):
+    message.reply("One way is to clone it to your local machine. Another way is to read it from Visual Studio Team")
+
+def chapter4(message):
+    message.reply("Awesome! Now, let's get relevant repositories on your local machine. Your org chart says you are in the Intune Team. You will probably need Intune-UX and Intune-Services. Please confirm with your manager that this is correct")
+
+def chapter5(message):
+    message.reply("Wonderful. So you need Intune-UX, Intune-Services, and Intune-SUPER-SECRET-REPOSITORIES")
+
+def chapter6(message):
+    message.reply("Please consult the following document for installing Intune-UX. Please let me know once you have finished")
+
+def chapter7(message):
+    message.reply("Based on previous experience with other employees, there’s an 80% that your error may be caused by the following")
--- oneboard/user.py
+++ oneboard/user.py
@@ -37,3 +37,26 @@ class User(object):
     def saveUserData(self):
         with open(userDataFilePath, 'wb') as userDataFile:
             pickle.dump(self, userDataFile, protocol=pickle.HIGHEST_PROTOCOL)
+
+    @staticmethod
+    def extract_name(message):
+        name = []
+        name = re.findall('.*is\s(\w+).*', message.lower())
+        if not name:
+            name = re.findall('.*im\s(\w+).*', message.lower())
+        if not name:
+            name = re.findall('.*\'m\s(\w+).*', message.lower())
+        if not name:
+            name = re.findall('.*am\s(\w+).*', message.lower())
+        if not name:
+            name = re.findall('.*call\sme\s(\w+).*', message.lower())
+        if not name:
+            name = re.findall('.*known\sas\s(\w+).*', message.lower())
+        if not name:
+            name = re.findall('.*\'s\s(\w+).*', message.lower())
+        if not name and len(message.split()) == 1:
+            return message.title()
+        if name:
+            return name[0].title()
+        else:
+            return [] 
